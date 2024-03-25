d67d4e4da2ae46d296ec992b3055e62af4eb2da6
polat <polatbekbeknazarov2003@gmail.com>
2024-03-26 03:49:24 +0500

    Changed asgi configurations

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
+import users.routing
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from users.middlewares.authMiddleware import JwtAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        JwtAuthMiddlewareStack(
+            URLRouter(users.routing.websocket_urlpatterns),
+        )
+    )
+})
