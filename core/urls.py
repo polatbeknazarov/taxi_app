62215409faf039ec2ef56d041a698e3abedcc918
polat <polatbekbeknazarov2003@gmail.com>
2024-03-27 00:27:07 +0500

    Test paths

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from line.views import TestAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls')),
+    path('api/v1/test/', TestAPIView.as_view()),
 ]
