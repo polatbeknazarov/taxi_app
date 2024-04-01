23d5badb39c12f59ae9a03c839bb873044124ab3
polat <polatbekbeknazarov2003@gmail.com>
2024-04-01 10:00:57 +0500

    Updated order and line models admin

--- line/admin.py
+++ line/admin.py
@@ -5,3 +5,5 @@ from line.models import Line
 @admin.register(Line)
 class LineAdmin(admin.ModelAdmin):
     list_display = ('id', 'driver', 'status', 'joined_at',)
+    search_fields = ('driver',)
+    list_per_page = 30
--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from orders.models import Order
 
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'from_city', 'to_city', 'phone_number', 'address', 'created_at', 'in_search',)
+    list_editable = ('from_city', 'to_city', 'phone_number', 'in_search')
+    search_fields = ('phone_number', 'address',)
+    list_per_page = 30
