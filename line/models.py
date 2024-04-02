782e44988d6823ec6b1c30d67be65be6c4bda294
polat <polatbekbeknazarov2003@gmail.com>
2024-04-03 02:48:20 +0500

    Updated model "Line"

--- line/models.py
+++ line/models.py
@@ -6,7 +6,22 @@ User = get_user_model()
 
 
 class Line(models.Model):
+    CITY_CHOICES = [
+        ('NK', 'Нукус'),
+        ('SB', 'Шымбай'),
+    ]
+
     driver = models.ForeignKey(User, on_delete=models.CASCADE)
+    from_city = models.CharField(
+        max_length=2,
+        choices=CITY_CHOICES,
+        blank=False,
+    )
+    to_city = models.CharField(
+        max_length=2,
+        choices=CITY_CHOICES,
+        blank=False,
+    )
     joined_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
 
