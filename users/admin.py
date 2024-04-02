26d5b40a2d9cfb9cd34e0cd46fc43828bbd4b24e
polat <polatbekbeknazarov2003@gmail.com>
2024-04-03 02:49:24 +0500

    Created 'User' model admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'phone_number',
+        'first_name',
+        'last_name',
+        'date_joined',
+        'is_active',
+    )
+    list_editable = ('is_active',)
+    search_fields = (
+        'username',
+        'phone_number',
+        'first_name',
+        'last_name',
+    )
+    list_per_page = 30
