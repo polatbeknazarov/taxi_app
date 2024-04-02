f9683f8c662e9ae54e5eebff05c802a4b643fa9d
polat <polatbekbeknazarov2003@gmail.com>
2024-04-03 02:49:38 +0500

    Created a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+998933640767'. Up to 15 digits allowed.",
+    )
+
+    phone_number = models.CharField(
+        max_length=18,
+        validators=[phone_regex,],
+        blank=False,
+    )
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
