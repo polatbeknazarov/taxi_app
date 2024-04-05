935498ede63b3c9cff451102e2ab4b8037c16508
polat <polatbekbeknazarov2003@gmail.com>
2024-04-05 20:09:37 +0500

    Added driver order history apiview

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,13 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from orders.models import Order
-from orders.serializers import OrderSerizlizer
+from orders.models import OrdersHistory
+from orders.serializers import OrderSerializer
 
 
 class OrderAPIView(APIView):
     def post(self, request):
-        serializer = OrderSerizlizer(data=request.data)
+        serializer = OrderSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
             serializer.save()
@@ -18,3 +18,15 @@ class OrderAPIView(APIView):
                     'detail': 'Order successfully created.'
                 }, status=status.HTTP_201_CREATED
             )
+
+
+class OrdersHistoryList(APIView):
+    def get(self, request, user_id):
+        orders_history = OrdersHistory.objects.filter(driver_id=user_id)
+        orders_data = []
+
+        for order_history in orders_history:
+            order_serializer = OrderSerializer(order_history.client)
+            orders_data.append(order_serializer.data)
+
+        return Response(orders_data, status=status.HTTP_200_OK)
